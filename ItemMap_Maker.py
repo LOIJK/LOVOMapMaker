b9ef50480489de44b942508d070ceaee0578e449
LOIJK <109383065+LOIJK@users.noreply.github.com>
2023-07-31 20:14:30 +0200

    Update ItemMap_Maker.py

--- ItemMap_Maker.py
+++ ItemMap_Maker.py
@@ -3,6 +3,7 @@ from tkinter import font
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import ttk
+from tkinter import scrolledtext
 from time import sleep
 import threading
 from threading import Thread
@@ -41,7 +42,7 @@ from PIL import Image
 ########################################################################################################################
 
 # Huidige versie van het script.
-versionnr = "1.0"
+versionnr = "1.1"
 
 # Verkrijg datum in het format YY-MM-DD zonder de -'s
 today = datetime.now().strftime("%y%m%d")
@@ -151,7 +152,66 @@ def update_to_latest_version():
 # Voer de functie update_to_latest_version uit wanneer het script start.
 update_to_latest_version()
 
+########################################################################################################################
+# Programmering voor speciale karakters
+########################################################################################################################
 
+# Functie om te checken voor speciale karakters
+def has_special_characters(input_str):
+    # Using regex to check for special characters
+    return bool(re.search(r"[!@#$%^&*(),?\":{}|<>_]", input_str))
+
+
+########################################################################################################################
+# Programmering voor progress bar voor downloaden scripts, vormgeving, huisstijlhandboek en vormgeving.
+# Maak een dubbele command van progressbar
+########################################################################################################################
+
+
+def progressbar():
+    # Maak scherm aan voor progressbar
+    progress_window = tk.Toplevel()
+    progress_window.title("Bestanden downloaden...")
+    progress_window.geometry("350x40")  # Eerste getal is breedte, tweede getal is hoogte
+
+    # Create an indeterminate progress bar
+    progress_bar = ttk.Progressbar(progress_window, mode='indeterminate')
+    progress_bar.pack(ipadx=350, ipady=40)
+
+    # Start the download process
+    progress_bar.start(20)  # Start the indeterminate progress bar animation
+
+
+
+########################################################################################################################
+#Programmering voor downloaden en uitpakken zip file
+########################################################################################################################
+
+def download_zip_file(download_link, folder_path):
+    # Download the zip file
+    zip_file = os.path.join(folder_path, "download.zip")
+    try:
+        response = requests.get(download_link)
+    except Exception as e:
+        logger.error(f'Fout bij het downloaden van het zip bestand: {e}')
+        return False
+
+    # Write the zip file to the folder
+    with open(zip_file, "wb") as f:
+        f.write(response.content)
+
+    # Extract the zip file in the folder
+    try:
+        zip_ref = zipfile.ZipFile(zip_file, "r")
+        zip_ref.extractall(folder_path)
+        zip_ref.close()
+    except Exception as e:
+        logger.error(f'Fout bij het uitpakken van het zip bestand: {e}')
+        return False
+
+    # Remove the zip file
+    os.remove(zip_file)
+    return True
 
 ########################################################################################################################
 #Programmering voor voer een titel in + gedeelte thema wijzigen
@@ -164,10 +224,15 @@ def run_themescript():
     folder_path = filedialog.askdirectory()
 
     # Verkrijg de naam, titel en categorie van de invulvelden/keuzeschermen
-    nameeditor = nameeditor_entry.get() 
+    nameeditor = nameeditor_entry.get()
     title = title_entry.get()
     category = selected_category.get()
     
+    # Check for special characters in the input fields
+    if has_special_characters(nameeditor) or has_special_characters(title) or has_special_characters(category):
+        messagebox.showerror("Error", "Gebruik geen speciale tekens in de invoervelden.")
+        return
+
     # Log de waardes van de naam, titel en categorie van de invulvelden/keuzeschermen
     logger.debug(f'Obtained values: name={nameeditor}, title={title}, category={category}')
 
@@ -185,29 +250,8 @@ def run_themescript():
         folder_path = os.path.join(folder_path, folder_name)
         os.makedirs(folder_path)
 
-        # download het zip-bestand
-        zip_file = os.path.join(folder_path, "download.zip")
-        try:
-            response = requests.get(download_link)
-        except Exception as e:
-            logger.error(f'Error downloaden zip file: {e}')
-            return
-
-        # schrijf het zip-bestand naar de map
-        with open(zip_file, "wb") as f:
-            f.write(response.content)
-
-        # pak het zip-bestand uit in de map
-        try:
-            zip_ref = zipfile.ZipFile(zip_file, "r")
-            zip_ref.extractall(folder_path)
-            zip_ref.close()
-        except Exception as e:
-            logger.error(f'Error uitpakken zip file: {e}')
-            return
-
-        # verwijder het zip-bestand
-        os.remove(zip_file)
+        # Download the zip file
+        download_zip_file(download_link, folder_path)
 
         for root, dirs, files in os.walk(f"{folder_path}/1. PROJECT"):
             for file in files:
@@ -220,7 +264,7 @@ def run_themescript():
         logger.debug(f'Uitpakken bestanden naar: {folder_path}')
 
         # laat een bericht zien om aan te geven dat het item succesvol is aangemaakt
-        messagebox.showinfo("Vormgeving succesvol aangemaakt!", "Locatie en mapnaam is te vinden op: " + folder_path)
+        messagebox.showinfo("Map succesvol aangemaakt!", "Locatie en mapnaam is te vinden op: " + folder_path)
     else:
         # Laat een error bericht zien wanneer de naam, titel en/of categorie van de invulvelden/keuzeschermen niet zijn ingevuld.
         messagebox.showerror("Error", "Voer de naam van de editor/redacteur in, een titel voor de map en selecteer een thema. \n\nControleer of je deze gegevens hebt ingevuld.")
@@ -230,19 +274,31 @@ def run_themescript():
 
 
 ########################################################################################################################
-#Programmering venster aanmaken + converters
+#Programmering venster aanmaken voor converters, hulpmiddelen en vormgeving
 ########################################################################################################################
 
 progress_bar = None
 
-def open_window_externevideoconvert():
-    global progress_bar
+def open_window_hulpmiddelen():
+
     # Create a new window
     convert_window = tk.Toplevel()
     convert_window.title("Bestanden converteren")
-    convert_window.geometry("380x200")
+    convert_window.geometry("380x260") # Eerste getal is breedte, tweede getal is hoogte
     convert_font = tk.font.Font(family='Arial', size=12)
     convert_window.option_add('*font', convert_font)
+
+    # Maak een label om uit te leggen: vormgeving downloaden
+    vormgeving = tk.Label(convert_window, text="Bij de hulpmiddelen zitten:\n fonts, mogrts, export presets")
+    vormgeving.pack(padx=5, pady=5)
+
+    # Maak een knop om de vormgeving te downloaden
+    vormgeving_button = tk.Button(convert_window, text="Download hulpmiddelen", command=lambda: download_overigeitems("Vormgeving", download_links["Vormgeving"]))
+    vormgeving_button.pack(pady=5)
+
+    # Maak een label om uit te leggen: vormgeving downloaden
+    convert = tk.Label(convert_window, text="Het volgende is nog in beta!\nHieronder kun je video's converteren\n naar LOVO format")
+    convert.pack(padx=5, pady=5)
 
     # Create a button for converting to the LOVO TV preset
     convert_button = tk.Button(convert_window, text="Converteer video naar LOVO TV Preset", command=run_convert_script)
@@ -252,7 +308,7 @@ def open_window_externevideoconvert():
     convert_button.pack(pady=5)
 
 def run_convert_script():
-    global progress_bar
+
     try:
         # Open a file dialog to select the file to convert
         root.filename = filedialog.askopenfilename(initialdir = "/", title = "Selecteer een bestand", filetypes = (("alle bestanden", "*.*"), ("MP4 bestanden", "*.mp4"), ("AVI bestanden", "*.avi"), ("MKV bestanden", "*.mkv")))
@@ -272,24 +328,21 @@ def run_convert_script():
 
         # Define the ffmpeg command to convert the file
         command = "ffmpeg -i {} -vcodec h264 -s 1920x1080 -r 25 -pix_fmt yuv420p -b:v 6M -profile:v main -level 4.1 -color_primaries bt709 -color_trc bt709 -colorspace bt709 -x265-params \"colorprim=bt709:transfer=bt709:colorspace=bt709\" -acodec aac -ar 48000 -ac 2 -b:a 192k -metadata:s:a:0 language=eng -metadata:s:a:0 title=\"Stereo\" -map_metadata 0 -map_chapters 0 -movflags +faststart {}_convertLOVOTVPreset.mp4".format(root.filename, root.filename)
-        progress_bar.start()
         def run():
             subprocess.run(command, shell=True, check=True)
-            progress_bar.stop()
             output_field.delete(1.0, tk.END)
             output_field.insert(tk.END, "{}".format(root.filename))
             winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
         t = Thread(target=run)
         t.start()
     except Exception as e:
-        progress_bar.stop()
         # Log the error message to the log file
         logger.exception(e)
         # Show an error message to the user
         messagebox.showerror("Foutmelding", "Er is een fout opgetreden tijdens het converteren van het bestand. Raadpleeg het logbestand voor meer informatie.")
 
 def run_convert_script_with_intro():
-    global progress_bar
+
     try:
         # Open a file dialog to select the file to convert
         root.filename = filedialog.askopenfilename(initialdir = "/", title = "Selecteer een bestand", filetypes = (("alle bestanden", "*.*"), ("MP4 bestanden", "*.mp4"), ("AVI bestanden", "*.avi"), ("MKV bestanden", "*.mkv")))
@@ -309,17 +362,14 @@ def run_convert_script_with_intro():
         intro_video = "Assets/ThemaBumper.mov"
         # Define the ffmpeg command to concatenate the intro video and the selected video 
         command = "ffmpeg -i {} -i {} -filter_complex \"[0:v]trim=start=4.15,format=yuva420p[intro];[intro][1:v]blend=all_mode='overlay':all_opacity=0.8[v]\" -map \"[v]\" -c:v libx264 -b:v 6M -r 25 -pix_fmt yuv420p -profile:v main -level 4.1 -color_primaries bt709 -color_trc bt709 -colorspace bt709 -x265-params \"colorprim=bt709:transfer=bt709:colorspace=bt709\" -acodec aac -ar 48000 -ac 2 -b:a 192k -metadata:s:a:0 language=eng -metadata:s:a:0 title=\"Stereo\" -map_metadata 0 -map_chapters 0 -movflags +faststart {}_convertLOVOTVPreset.mp4".format(intro_video, root.filename, root.filename)
-        progress_bar.start()
         def run():
             subprocess.run(command, shell=True, check=True)
-            progress_bar.stop()
             output_field.delete(1.0, tk.END)
             output_field.insert(tk.END, "{}".format(root.filename))
             winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
         t = Thread(target=run)
         t.start()
     except Exception as e:
-        progress_bar.stop()
         # Log the error message to the log file
         logger.exception(e)
         # Show an error message to the user
@@ -350,31 +400,7 @@ def download_overigeitems(item_name, download_link):
     os.makedirs(folder_path)
 
     # Download the zip file
-    zip_file = os.path.join(folder_path, "download.zip")
-    try:
-        response = requests.get(download_link)
-    except Exception as e:
-        logger.error(f'Error downloading zip file: {e}')
-        return
-
-    # Write the zip file to the folder
-    with open(zip_file, "wb") as f:
-        f.write(response.content)
-
-    # Extract the zip file to the folder
-    try:
-        zip_ref = zipfile.ZipFile(zip_file, "r")
-        zip_ref.extractall(folder_path)
-        zip_ref.close()
-    except Exception as e:
-        logger.error(f'Error extracting zip file: {e}')
-        return
-
-    # Delete the zip file
-    os.remove(zip_file)
-
-    # Log the folder where the files are extracted to
-    logger.debug(f'Extracting files to: {folder_path}')
+    download_zip_file(download_link, folder_path)
 
     # Show a message that the item was successfully created
     messagebox.showinfo(f"{item_name} succesvol aangemaakt!", "Locatie en mapnaam is te vinden op: " + folder_path)
@@ -419,7 +445,7 @@ def open_folder():
 
 root = tk.Tk()
 root.title("LOVO Map Maker")
-root.geometry("400x665") # Eerste getal is breedte, tweede getal is hoogte
+root.geometry("400x505") # Eerste getal is breedte, tweede getal is hoogte
 font = tk.font.Font(family='Arial', size=12) # Semibold werkt niet, bold niet gebruiken
 root.option_add('*font', font)
 
@@ -436,26 +462,17 @@ titlelabel = tk.Label(root, text="Titel van item:")
 titlelabel.pack()
 
 title_entry = tk.Entry(root)
-title_entry.pack(padx=5, pady=5)
+title_entry.pack(padx=5, pady=(5, 10))
 
 selected_category = tk.StringVar(root)
 selected_category.set("Selecteer thema") # instellen als standaardwaarde
 
-categories = ["Amusement", "Archief", "Cultuur", "Natuur", "Nieuws", "Politiek", "Sport"]
+categories = ["Amusement", "Archief", "Cultuur", "Natuur", "Nieuws", "Politiek", "Sport", "AanTafelMetClaudy", "GezondOisterwijk", "Special"]
 category_menu = ttk.Combobox(root, textvariable=selected_category, values=categories)
 category_menu.pack(pady=5)
 
 button = tk.Button(root, text="Maak map aan", command=run_themescript)
 button.pack(pady=5)
-
-# Maak een knop met de naam "Vormgeving"
-label = tk.Label(root, text="Overige hulpmiddelen:")
-label.pack(pady=(30, 5))
-
-# Maak een knop om de vormgeving te downloaden
-vormgeving_button = tk.Button(root, text="Download vormgeving", command=lambda: download_overigeitems("Vormgeving", download_links["Vormgeving"]))
-vormgeving_button.pack(pady=5)
-vormgeving_button.pack(pady=5)
 
 # Maak een text veld aan dat o.a. het net aangemaakte folder_path kan laten zien.
 output_field = tk.Text(root, width=30, height=5)
@@ -466,14 +483,7 @@ output_field.pack(padx=10, pady=10)
 copy_button = tk.Button(root, text="Open map locatie", command=open_folder)
 copy_button.pack(padx=10, pady=10)
 
-# Converteer venster
-convert_button = tk.Button(root, text="Video's converteren", command=open_window_externevideoconvert)
-convert_button.pack(pady=5)
-
-# Create a progress bar
-progress_bar = ttk.Progressbar(root, mode='determinate',length=275)
-progress_bar.pack(pady=20)
-
+# Knop ?
 button = tk.Button(root, text="?", width=3, height=1, command=run_vraagteken)
 button.pack(side="bottom", anchor="se", padx= 5, pady=5)
 
@@ -481,7 +491,12 @@ button.pack(side="bottom", anchor="se", padx= 5, pady=5)
 #handleiding_button = tk.Button(root, text="Handleiding", command=download_handleiding)
 #handleiding_button.pack(side="bottom", anchor="se", padx= 5, pady=5)
 
+# Knop huisstijlhandboek
 huisstijlhandboek_button = tk.Button(root, text="Download huisstijlhandboek", command=lambda: download_overigeitems("Huisstijlhandboek", download_links["Huisstijlhandboek"]))
 huisstijlhandboek_button.pack(side="bottom", anchor="se", padx= 5, pady=5)
+
+# Knop hulpmiddelen
+convert_button = tk.Button(root, text="Hulpmiddelen", command=open_window_hulpmiddelen)
+convert_button.pack(side="bottom", anchor="se", padx= 5, pady=5)
 
 root.mainloop()
